880f6c7ef1ff62bba7d6f50c20c2926d36c91b35
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2010-04-02 14:51:46 -0400

    Added base setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="webdevcrawler",
+      version="0.1",
+      install_requires=[
+          'django',
+          'distribute',
+      ],
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+
+)
