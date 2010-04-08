18a8de9108938b9344b6c68a2e2f0f493c442b0a
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2010-04-08 12:17:12 -0400

    Added static content directory

--- src/webdevcrawler/settings.py
+++ src/webdevcrawler/settings.py
@@ -34,7 +34,8 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__),
+        os.pardir, os.pardir, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
--- src/webdevcrawler/urls.py
+++ src/webdevcrawler/urls.py
@@ -1,21 +1,30 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
+        # Example:
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+        # to INSTALLED_APPS to enable admin documentation:
+        (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+        # Uncomment the next line to enable the admin:
+        (r'^admin/', include(admin.site.urls)),
 
-    (r'^accounts/login/$', 'django_cas.views.login'),
-    (r'^accounts/logout/$', 'django_cas.views.logout'),
+        (r'^accounts/login/$', 'django_cas.views.login'),
+        (r'^accounts/logout/$', 'django_cas.views.logout'),
 
-    (r'^', include('webdevcrawler.crawler.urls')),
-)
+        (r'^', include('webdevcrawler.crawler.urls')),
+        )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+            (r'^{0}/(?P<path>.*)$'.format(settings.MEDIA_URL.strip('/')),
+                'django.views.static.serve',
+                {'document_root': settings.MEDIA_ROOT }),
+            )
+
